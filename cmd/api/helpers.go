38b0ae64d752d2d649b88ec46917e3b5ed2c3b17
PirasL <leo.piras@outlook.com>
2024-11-26 15:27:55 +0100

    Initial commit

---
+++ cmd/api/helpers.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func readJSON(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func writeJSON(w http.ResponseWriter, r *http.Request, status int, message string) {}
